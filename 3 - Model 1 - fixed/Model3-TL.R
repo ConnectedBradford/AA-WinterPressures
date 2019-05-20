c39474f6a80a467b5829682c0156aceb0d76b169
Tom Lawton <tlawton@gmx.de>
2019-05-20 19:43:59 +0100

    Updated to add the "bed" resource (for overall capacity) and fake sex/specialty code.
    Former-commit-id: 52b9a95fc166aaa3d77145b86cd66563d97eb412

--- 3 - Model 1 - fixed/Model3-TL.R
+++ 3 - Model 1 - fixed/Model3-TL.R
@@ -70,12 +70,16 @@ iterate  <- function() {
 patient<- trajectory() %>%
   set_attribute("patient_id", iterate()) %>%
   set_attribute("segment", 1) %>%
+  set_attribute("sex", function() {sample(c(1,2),1)}) %>% 
+  set_attribute("specialtycode", function() {sample(c(100,101,120,300,301,320),1)}) %>% 
+  seize("bed") %>% 
   select(function() {patients[["data"]][[get_attribute(env,"patient_id")]][["req"]][get_attribute(env,"segment")]}) %>%
   seize_selected() %>% 
   timeout(function() {patients[["data"]][[get_attribute(env,"patient_id")]][["dur"]][get_attribute(env,"segment")]}) %>%
   release_selected() %>% 
   set_attribute("segment",1,"+") %>% 
-  rollback(5,check = function() {!is.na(patients[["data"]][[get_attribute(env,"patient_id")]][["req"]][get_attribute(env,"segment")])})
+  rollback(5,check = function() {!is.na(patients[["data"]][[get_attribute(env,"patient_id")]][["req"]][get_attribute(env,"segment")])}) %>% 
+  release("bed")
 
 
 ## create resources
@@ -83,7 +87,8 @@ patient<- trajectory() %>%
 env <-
   simmer("hospital") %>% 
   # add_resource(wards,capacity=Inf,queue_size=0,queue_size_strict=TRUE) %>% 
-  add_generator("Patient", patient, at(patients$at))
+  add_resource("bed",capacity=Inf) %>% 
+  add_generator("Patient", patient, at(patients$at), mon=2)
 
 
 for (ward in wards) {
@@ -100,3 +105,7 @@ print("* run *")
 #print(plot(get_mon_resources(env),steps=TRUE))
 
 print(plot(get_mon_resources(env),"usage",c("5 BRI","ENT DCU BRI","19 Disch Lounge BRI","12 BRI","11 BRI","8 BRI", "23 BRI", "27 BRI","22 BRI"),items="system",steps=TRUE))
+
+resources<-get_mon_resources(env)
+arrivals<-get_mon_arrivals(env)
+attributes<-get_mon_attributes(env)
