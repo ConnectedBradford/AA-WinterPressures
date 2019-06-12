c5e9942ae4d47d2db47c11b0994b2f905657411c
Tom Lawton <tlawton@gmx.de>
2019-06-12 10:46:12 +0100

    Add ward data
    Former-commit-id: 587d30ba43229f0e9c93ecb3477b1d3c421df34f

---
+++ 4 - Model 2 - real data/Wards.R
@@ -0,0 +1,11 @@
+
+wards<-read.csv("../4 - Model 2 - real data/Wards.csv")
+
+##dataframe - Ward, Beds, Restrictions, Surge
+##Ward = name
+##Beds = number of beds when open
+##Restrictions:
+## F - female only
+## P - paeds only
+##Surge:
+## 1 - surge area (opening defined in model, starts shut)
