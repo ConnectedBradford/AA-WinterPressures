37bc893c3da9fee7496ea9e44e6f02f8030f7104
Tom Lawton <tlawton@gmx.de>
2019-07-11 10:59:26 +0100

    Some r-simmer tests
    Former-commit-id: ae53947ea3beddeb45f668080326281888284285

---
+++ R-simmer tests/parallel test.R
@@ -0,0 +1,47 @@
+library(parallel)
+
+
+
+
+
+
+                 
+                 
+  simmer_wrapper<-function(i) {
+    
+    patient <- trajectory("patients' path") %>%
+      ## add an intake activity 
+      seize("nurse", 1) %>%
+      timeout(function() rnorm(1, 15)) %>%
+      release("nurse", 1) %>%
+      ## add a consultation activity
+      seize("doctor", 1) %>%
+      timeout(function() rnorm(1, 20)) %>%
+      release("doctor", 1) %>%
+      ## add a planning activity
+      seize("administration", 1) %>%
+      timeout(function() rnorm(1, 5)) %>%
+      release("administration", 1)
+    
+    
+  simmer("SuperDuperSim") %>%
+    add_resource("nurse", 1) %>%
+    add_resource("doctor", 2) %>%
+    add_resource("administration", 1) %>%
+    add_generator("patient", patient, function() rnorm(1, 10, 2)) %>%
+    run(80) %>%
+    wrap()
+  }
+  
+ #envs <- mclapply(1:100,simmer_wrapper)
+  
+  
+  
+  library("doParallel")
+  registerDoParallel(cores=8)
+  
+  trials<-2
+  envs <- foreach(icount(trials), .combine=c, .noexport=c(),.export=c(), .packages=c("simmer"), .verbose=TRUE) %do% {
+    simmer_wrapper(1)
+  }
+  
---
+++ R-simmer tests/renege rollback test.R
@@ -0,0 +1,59 @@
+library(simmer)
+
+set.seed(1014)
+
+
+
+sub1<-trajectory() %>% 
+  log_("sub1A") %>% 
+  timeout(1) %>% 
+  log_("sub1B") %>% 
+  timeout(1) %>% 
+  log_("sub1C") %>% 
+  timeout(1) %>% 
+  log_("sub1D") %>% 
+  rollback(5)
+
+sub2<-trajectory() %>% 
+  log_("sub2A") %>% 
+  timeout(3) %>% 
+  rollback(3)
+
+sub3<-trajectory() %>% 
+  log_("sub3A") %>% 
+  timeout(1) %>% 
+  log_("sub3B") %>% 
+  timeout(1) %>% 
+  release_all("counter1") %>% 
+  leave(1)
+
+renege<-trajectory() %>% 
+  log_("renege") %>% 
+  rollback(3)
+
+rejected<-trajectory() %>% 
+  log_("rejected") %>% 
+  rollback(3)
+
+customer <-
+  trajectory("Customer's path") %>%
+  log_("A") %>% 
+  timeout(1) %>% 
+  seize("counter1",reject=rejected,continue=FALSE) %>% 
+  log_("B") %>% 
+  log_("C") %>% 
+  renege_in(2,renege) %>% 
+  branch(function() {sample(1:3,1)},TRUE,sub1,sub2,sub3)
+
+
+bank <-
+  simmer("bank") %>%
+  add_resource("counter1", 1) %>%
+  add_resource("counter2", 1) %>%
+  add_generator("Customer", customer, function() {c(0, rexp(4, 1/10),0,0,0, -1)})
+
+bank %>% run(until = 400)
+
+
+##conclusion - rollback in a renege trajectory uses the "renegein" as point zero and ignores what the arrival has done since
+## rollback in a seize reject trajectory works the same (seize is point zero)
