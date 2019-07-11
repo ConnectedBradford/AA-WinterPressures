edd4acec8b59d82e0552ce2cf34c4124415aff6e
Tom Lawton <tlawton@gmx.de>
2019-07-11 12:20:33 +0100

    Add full wards with limits
    Former-commit-id: 67caa01eb625c5c6ec8b8b50c56c8531824d6acd

--- 4 - Model 2 - real data/Wards.R
+++ 4 - Model 2 - real data/Wards.R
@@ -10,4 +10,7 @@ wards<-read.csv("../4 - Model 2 - real data/Wards.csv", header=TRUE,sep=",")
 ##Surge:
 ## 1 - surge area (opening defined in model, starts shut)
 
+wards$Ward<-as.character(wards$Ward)
+#for some reason we end up with the ward names as a factor variable which causes trouble when it's used in the "put the patient anywhere" rule
+
 print("* Wards Loaded *")
