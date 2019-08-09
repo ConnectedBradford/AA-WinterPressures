c34146217ce7e49d3622b68b654d9ebb6f0ecb23
Tom Lawton <tlawton@gmx.de>
2019-08-09 18:16:16 +0100

    testing substitutions
    Not reliable - need to use rollbacks

---
+++ R-simmer tests/other loops test.R
@@ -0,0 +1,60 @@
+library(simmer)
+
+set.seed(1014)
+
+customer2<-
+  trajectory() %>% 
+  log_("C2")
+
+subJoin<- trajectory("Joined") %>% 
+  log_("item1") %>% 
+  log_("item2") %>% 
+  log_("item3")
+
+reneged <-
+  trajectory("renege") %>% 
+  log_("renege")
+
+sub1<-trajectory() %>% 
+  log_("sub1A") %>% 
+  log_("sub1B") %>% 
+  join(subJoin) %>% ##for rollbacks, join counts as however many tasks are in the joined trajectory, not just one
+  rollback(6)
+
+sub2<-trajectory() %>% 
+  log_("sub2A") %>% 
+  rollback(3)
+
+sub3<-trajectory() %>% 
+  log_("sub3A") %>% 
+  log_("sub3B") %>% 
+  leave(1)
+
+
+
+customer <-
+  trajectory("Customer's path") %>%
+  log_("A") %>% 
+  log_("B") %>% 
+  log_("C") %>% 
+  branch(function() {sample(1:3,1)},TRUE,sub1,sub2,sub3)
+
+subX<-trajectory() %>% 
+  log_("subX") %>% 
+  leave(1)
+
+sub3[3]<-trajectory() %>% branch( function() {1},continue=FALSE,subX)
+
+
+##subX never entered, unless the whole subX part is moved to above "customer"
+
+
+bank <-
+  simmer("bank") %>%
+  add_resource("counter1", 1) %>%
+  add_resource("counter2", 1) %>%
+  add_generator("Customer", customer, function() {c(0, rexp(4, 1/10),0,0,0, -1)})
+
+#bank %>% run(until = 400)
+
+print(plot(customer),verbose=TRUE)
